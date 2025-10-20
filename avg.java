d9ca7607d173e246a43d10acd30f8e7eff2f3b76
ShamithChandra <tsshamithchandra@gmail.com>
2025-10-20 17:40:55 +0530

    add avg of array prg

---
+++ avg.java
@@ -0,0 +1,16 @@
+public class avg {
+    public static void main(String args[]){
+        int sum=0;
+        int avg=0;
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        for(int i=0;i<9;i++){
+            sum+=arr[i];
+            avg=sum/9;
+
+
+        }
+        System.out.println("avg of array elements is :"+avg);
+
+    }
+    
+}
