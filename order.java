f4693f4546cd594c3c059a31f6069443792d5403
ShamithChandra <tsshamithchandra@gmail.com>
2025-10-23 13:47:11 +0530

    add order program

---
+++ order.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class order {
+    public static void main(String args[]){  // restaurant bill calculator 
+        Scanner sc=new Scanner(System.in);
+        String item;
+        int quantity;
+        float price;
+        System.out.println("What would u like to order?");
+        item=sc.nextLine();
+        System.out.println("Quantity:");
+        quantity=sc.nextInt();
+        System.out.println("What is the price $ :");
+        price=sc.nextFloat();
+
+        if(quantity>1){
+            price*=quantity;
+            System.out.println("your order:"+item);
+            System.out.println("quantity:"+quantity);
+            System.out.println("total price:"+price);
+        }
+        else{
+            System.out.println("your order:"+item);
+            System.out.println("quantity:"+quantity);
+            System.out.println("total prize:"+price+"$");
+
+        }
+        sc.close();
+
+
+    }
+    
+}
