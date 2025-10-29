5429f9db40dcbc90bb08142462c06425ffeada18
ShamithChandra <tsshamithchandra@gmail.com>
2025-10-29 19:36:59 +0530

    add bubbble sort prg

---
+++ bubble.java
@@ -0,0 +1,19 @@
+public class bubble{
+    public static void main(String args[]){
+        int arr[]={7,4,6,2,9,0};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                int temp;
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+       
+    }
+}
